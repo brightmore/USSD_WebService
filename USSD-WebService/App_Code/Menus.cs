041115bb4171df95f47dd9707aae79d466f0725d
mohamadjavad bahmani <mohamadjavad.bahmani@gmail.com>
2017-12-07 14:59:10 +0330

    Add files via upload

---
+++ USSD-WebService/App_Code/Menus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Menus
+/// </summary>
+public class Menus
+{
+    public Menus()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    #region Menus
+    public static string Enter = "\n";
+    #region 1
+    public static string First = "1-item" + Enter + "2-item" + Enter + "3-item" + Enter + "4-item  " + Enter + "5-item  ";
+    public static string WrongAnswer = " error , please try again";
+    #endregion
+}
---
+++ USSD-WebService/App_Code/USSD.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for USSD
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class USSD : System.Web.Services.WebService
+{
+    static string status = string.Empty;
+    static string sid = string.Empty;
+    public USSD()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(UseHttpGet = true)]
+    public void ussd(string userid, string password, string MSISDN, string input, string SID)
+    {
+        if (SID.CompareTo(sid) != 0)
+        {
+            sid = SID;
+            status = String.Empty;
+        }
+        status = status + input;
+        if (input == "check input")
+        {
+            status = status.Replace("check input", "");
+            Continue(Menus.First);
+            return;
+        }
+   
+       
+        #region Wrong Answer
+        WrongAnswer();
+        return;
+        #endregion
+
+    }
+    private void Continue(string menu)
+    {
+        HttpContext.Current.Response.Output.WriteLine(menu);
+        HttpContext.Current.Response.AddHeader("Freeflow", "FC");
+        return;
+    }
+    private void End(string menu)
+    {
+        HttpContext.Current.Response.Output.WriteLine(menu);
+        HttpContext.Current.Response.AddHeader("Freeflow", "FB");
+        return;
+    }
+
+    private void WrongAnswer()
+    {
+        HttpContext.Current.Response.Output.WriteLine(Menus.WrongAnswer);
+        HttpContext.Current.Response.AddHeader("Freeflow", "FB");
+        return;
+    }
+
+}
